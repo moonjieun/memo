1a8418b44000501401d743bde90929bf05df67e4
moonjieun <sododls0330@naver.com>
2023-07-03 14:51:10 +0900

    메모장 만들기

---
+++ static/app.js
@@ -0,0 +1,41 @@
+function dispalyMemo(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  ul.appendChild(li);
+}
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(dispalyMemo);
+  console.log(jsonRes);
+}
+
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime(),
+      content: value,
+    }),
+  });
+  const jsonRes = await res.json();
+  readMemo();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
+
+readMemo();
